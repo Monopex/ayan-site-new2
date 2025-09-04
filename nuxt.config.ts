353e47e4250d24dd1567fe5d76e3bf496d26bd0f
Monopex <morter95@yandex.ru>
2025-09-05 00:21:29 +0500

    Add lang

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,26 @@ import { fileURLToPath } from 'node:url'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['shadcn-nuxt'],
+  modules: [
+    '@nuxtjs/i18n',
+    'shadcn-nuxt'
+  ],
   alias: {
     '@': fileURLToPath(new URL('./', import.meta.url)),
   },
   css: ['@/assets/css/app.css'],   // можно оставить @
   vite: { plugins: [tailwindcss(), tsconfigPaths()] },
+  i18n: {
+    strategy: 'prefix_except_default',
+    defaultLocale: 'ru',
+    locales: [
+      { code: 'ru', iso: 'ru-RU', name: 'Русский', file: 'ru.json' },
+      { code: 'kz', iso: 'kz-KZ', name: 'Қазақша', file: 'kz.json' },
+    ],
+    lazy: true,
+    langDir: 'locales',
+    vueI18n: './i18n.config.ts',
+  },
   shadcn: {
     prefix: '',
     componentDir: './components/ui',
